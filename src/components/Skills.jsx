d7c30185a2948902306d01cd454c50c5a585ab60
Millaray <millaray.painemil@outlook.cl>
2025-05-10 17:22:06 -0400

    inicio back

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,6 @@
 
 import { Col, Container, ProgressBar, Row } from 'react-bootstrap'
-import { FaBootstrap,FaHtml5, FaCss3Alt,FaReact,FaGitAlt, FaGithub, FaNodeJs } from 'react-icons/fa'
+import { FaBootstrap,FaHtml5, FaCss3Alt,FaReact,FaGitAlt, FaGithub, FaNode } from 'react-icons/fa'
 import { TiHtml5 } from 'react-icons/ti'
 import { SiReactbootstrap, SiAxios, SiJest, SiExpress, SiAdobephotoshop } from "react-icons/si";
 import { BiLogoPostgresql } from "react-icons/bi";
@@ -19,72 +19,83 @@ const Skills = () => {
           
           <Row className='justify-content-between px-5 py-2'>
             <Col xs={4} sm={3} md={3} lg={1}>
-              <FaHtml5 className='animatedIconHTML' size={60}/>
+              <FaHtml5 className='animatedIconHTML mx-2' size={60}/>
               <p className='iconTextTech'>Html 5</p>
             </Col>
 
             <Col xs={4} sm={3} md={3} lg={1}>
-              <FaCss3Alt className='animatedIconCSS ' size={60}/>
+              <FaCss3Alt className='animatedIconCSS mx-2' size={60}/>
               <p className='iconTextTech'>CSS 3</p>
             </Col>
             <Col xs={4} sm={3} md={3} lg={1}>
-              <IoLogoJavascript className='animatedIconJavascript ' size={60}/>
+              <IoLogoJavascript className='animatedIconJavascriptmx-2' size={60}/>
              <p className='iconTextTech'>JavaScript</p>
             </Col>
             <Col xs={4} sm={3} md={3} lg={1} >
-              <FaBootstrap className='animatedIconBootstrap ' size={60}/>
+              <FaBootstrap className='animatedIconBootstrap mx-2' size={60}/>
               <p className='iconTextTech'>Bootstrap</p>
             </Col>
         
             <Col xs={4} sm={3} md={3} lg={1}>
-              <FaReact className='animatedIconReact ' size={60}/>
+              <FaReact className='animatedIconReact mx-2' size={60}/>
               <p className='iconTextTech'>React</p>
             </Col>
 
             <Col xs={4} sm={3} md={3} lg={1}>
-              <IoLogoWordpress className='animatedIconWordpress ' size={60}/>
+              <IoLogoWordpress className='animatedIconWordpress mx-2' size={60}/>
              <p className='iconTextTech'>Wordpress</p>
             </Col>
                <Col xs={4} sm={3} md={3} lg={1}>
-              <SiAxios className='animatedIconAxios ' size={60}/>
+              <SiAxios className='animatedIconAxios mx-2' size={60}/>
               <p className='iconTextTech'>Axios</p>
             </Col>
             <Col xs={8} sm={3} md={3} lg={1}>
-              <SiReactbootstrap className='animatedIconReactB ' size={60}/>
+              <SiReactbootstrap className='animatedIconReactB mx-2' size={60}/>
              <p className='iconTextTech'>React Bootstrap</p>
             </Col>
         
          
           </Row>
           
-          <Row>
-            <Col>
+          <Row className='d-flex justify-content-between my-4' >
+            <Col xs={12} sm={3}>
               <h3>Desarrollo Backend</h3>
-                <FaNodeJs/>
-                <SiExpress/>
+              <Row  className='d-flex justify-content-around'>
+                <Col xs={4} sm={3} md={3} lg={1}>
+                  <FaNode className='animatedIconNode mx-2' size={60}/>
+                  <p className='iconTextTech'>Node.js</p>
+                </Col>
+                <Col xs={4} sm={3} md={3} lg={1}>
+                  <SiExpress className='animatedIconExpress mx-2' size={60}/>
+                  <p className='iconTextTech'>Express.js</p>
+                </Col>
+              </Row>
+          
+           
             </Col>
-            <Col>
+            <Col xs={12} sm={3} >
               <h3>Testing</h3>
                <SiJest/>
             </Col>
-            <Col>
+            <Col xs={12} sm={3}>
               <h3> Bases de datos</h3>
-              <BiLogoPostgresql/></Col>
+              <BiLogoPostgresql/>
+            </Col>
           </Row>
 
       
-        <Row>
-          <Col>
+        <Row  className='d-flex justify-content-between mb-4'>
+          <Col xs={12} sm={3}>
             <h3>Ofimática</h3>
               <PiMicrosoftWordLogoFill/>
               <SiAdobephotoshop/>
           </Col>
-          <Col>
+          <Col xs={12} sm={3}>
            <h3>Herramientas</h3>
             <FaGitAlt/>
             <FaGithub/>
           </Col>
-          <Col>    
+          <Col xs={12} sm={3}>    
           <h3>Diseño UX/UI</h3>
             <IoLogoFigma/>
           </Col>
