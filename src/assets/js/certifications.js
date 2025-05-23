f18082df50d9f701fcad385daa6546f20f64c54f
Millaray <millaray.painemil@outlook.cl>
2025-05-23 15:27:32 -0400

    cambio a pdf

--- src/assets/js/certifications.js
+++ src/assets/js/certifications.js
@@ -9,7 +9,7 @@ const certifications ={esp:[{
   },
   date:'Mayo - 2025',
   color:'#dff8d7',
-  url:"https://media.licdn.com/dms/image/v2/D4D22AQFvCu5vOwTj4w/feedshare-shrink_1280/B4DZa_.W.wHQAk-/0/1746977517828?e=1750291200&v=beta&t=J7dZmU5LJcTq0emGPLTAyZW89A5x55HR-54WnaeOdHI://media.licdn.com/dms/image/v2/D4D2DAQEV4aeC19oUzQ/profile-treasury-image-shrink_800_800/B4DZYlIb41HAAg-/0/1744379693880?e=1747944000&v=beta&t=ecD4UU8rGOuj00x3SY7lnKpE-2D_h9kbL6Hkj35P4Yg"
+  url:"/files/fundamentos-profesionales-ciberseguridad.pdf"
 
 }, {
   img:'/certifications/UPLA.png',
@@ -22,7 +22,7 @@ const certifications ={esp:[{
   },
   date:'Mayo - 2025',
   color:'#d7f8f4',
-  url:"https://media.licdn.com/dms/image/v2/D4D2DAQGiuXYxCHOHlg/profile-treasury-document-images_800/B4DZa17pPaGYAk-/1/1746809036334?e=1748476800&v=beta&t=2KASWJGOMpKXs1FV52rv4KyrS5Xo6S1ngQKOUyFKNpY"
+  url:"/files/Metodologias-Agiles.pdf"
 
 },{
   img:'/certifications/AIEP.png',
@@ -35,7 +35,7 @@ const certifications ={esp:[{
   },
   date:'Febrero - 2024',
   color:'#fae7e7',
-  url:"https://media.licdn.com/dms/image/v2/D4D2DAQFyCKvEFNZ8yw/profile-treasury-document-images_800/B4DZainpNUG8Ak-/1/1746485026141?e=1748476800&v=beta&t=M1yJ3EFjFSAyzMufdJKENpmw6DlFqLwzsKj_yEbNq6Y"
+  url:"/files/Wordpress.pdf"
 
 },{
   img:'/certifications/sence.png',
@@ -48,7 +48,7 @@ const certifications ={esp:[{
   },
   date:'Febrero - 2024',
   color:'#e4e9f8',
-  url:"https://media.licdn.com/dms/image/v2/D4D2DAQFLemEu-ayqow/profile-treasury-document-images_800/B4DZaimavEGsAk-/1/1746484704901?e=1748476800&v=beta&t=kR7u7z3rlfC58skiMDT6e-MQnDF97m6AmmLFxGyNH-o"
+  url:"/files/ciberseguridad-1.pdf"
 
 },{
   img:'/certifications/IBM.png',
@@ -61,7 +61,7 @@ const certifications ={esp:[{
   },
   date:'Enero - 2025',
   color:'#dfdfdf',
-  url:"https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~QBHGPG24IVQB/CERTIFICATE_LANDING_PAGE~QBHGPG24IVQB.jpeg"
+  url:"/files/UX-UI.pdf"
 
 },{
   img:'/certifications/IBM.png',
@@ -74,7 +74,7 @@ const certifications ={esp:[{
   },
   date:'Febrero - 2025',
   color:'#dfdfdf',
-  url:"https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~RPJQZRPNU4YW/CERTIFICATE_LANDING_PAGE~RPJQZRPNU4YW.jpeg"
+  url:"h/files/Bootstrap.pdf"
 
 },{
   img:'/certifications/Udemy.png',
@@ -86,7 +86,7 @@ const certifications ={esp:[{
   },
   date:'Febrero - 2024',
   color:'#f0def8',
-  url:"https://media.licdn.com/dms/image/v2/D4D2DAQGL25op3SRMxQ/profile-treasury-document-images_800/B4DZXN7suTH4Ak-/1/1742916735343?e=1748476800&v=beta&t=UUf75TOjz5Xx9Mz21w-H-hyvJNMi4QeYno8PmmFqJ0E"
+  url:"/files/Certificado_Excel.pdf"
 },{
   img:'/certifications/ADL.png',
   type:0,
@@ -97,7 +97,7 @@ const certifications ={esp:[{
   },
   date:'Noviembre - 2024',
   color:'#e5f8cc',
-  url:"https://media.licdn.com/dms/image/v2/D4D2DAQHucHNa-H8O2w/profile-treasury-document-images_800/B4DZXN72nxHYAk-/1/1742916776796?e=1748476800&v=beta&t=JSCpnA99AEJQWowFdKvjXJ_8ttWm1v-81UgdmIOc6l4"
+  url:"/files/Figma.pdf"
 
 }, {
   img:'/certifications/UPLA.png',
@@ -110,7 +110,7 @@ const certifications ={esp:[{
   },
   date:'Febrero - 2024',
   color:'#d7f8f4',
-  url:"https://media.licdn.com/dms/image/v2/D4D2DAQGK_9qbAjYmPA/profile-treasury-document-images_800/B4DZain.kbGsAk-/1/1746485113810?e=1748476800&v=beta&t=naudiot7OcnieSVxpnUQI-KYBuzGfBlLlXqeQq_bb0U"
+  url:"/files/Diplomado-UPLA.pdf"
 
 }, {
   img:'/certifications/AIEP.png',
@@ -123,7 +123,7 @@ const certifications ={esp:[{
   },
   date:'Abril - 2024',
   color:'#fae7e7',
-  url:"https://media.licdn.com/dms/image/v2/D4D2DAQHKha32mTdxQg/profile-treasury-document-images_800/B4DZainG7vGcAk-/1/1746484885385?e=1748476800&v=beta&t=yrNe_giJINM9XpEN5HCt5G7iWcVcptTJm5gQwF_NR-Y"
+  url:"/files/Diplomado-AIEP.pdf"
 
 }],
 
@@ -139,7 +139,7 @@ eng:[{
   },
   date:'May - 2025',
   color:'#dff8d7',
-  url:"https://media.licdn.com/dms/image/v2/D4D22AQFvCu5vOwTj4w/feedshare-shrink_1280/B4DZa_.W.wHQAk-/0/1746977517828?e=1750291200&v=beta&t=J7dZmU5LJcTq0emGPLTAyZW89A5x55HR-54WnaeOdHI://media.licdn.com/dms/image/v2/D4D2DAQEV4aeC19oUzQ/profile-treasury-image-shrink_800_800/B4DZYlIb41HAAg-/0/1744379693880?e=1747944000&v=beta&t=ecD4UU8rGOuj00x3SY7lnKpE-2D_h9kbL6Hkj35P4Yg"
+  url:"/files/fundamentos-profesionales-ciberseguridad.pdf"
 
 }, {
   img:'/certifications/UPLA.png',
@@ -152,7 +152,7 @@ eng:[{
   },
   date:'May - 2025',
   color:'#d7f8f4',
-  url:"https://media.licdn.com/dms/image/v2/D4D2DAQGiuXYxCHOHlg/profile-treasury-document-images_800/B4DZa17pPaGYAk-/1/1746809036334?e=1748476800&v=beta&t=2KASWJGOMpKXs1FV52rv4KyrS5Xo6S1ngQKOUyFKNpY"
+  url:"/files/Metodologias-Agiles.pdf"
 
 },{
   img:'/certifications/AIEP.png',
@@ -165,7 +165,7 @@ eng:[{
   },
   date:'February - 2024',
   color:'#fae7e7',
-  url:"https://media.licdn.com/dms/image/v2/D4D2DAQFyCKvEFNZ8yw/profile-treasury-document-images_800/B4DZainpNUG8Ak-/1/1746485026141?e=1748476800&v=beta&t=M1yJ3EFjFSAyzMufdJKENpmw6DlFqLwzsKj_yEbNq6Y"
+  url:"/files/Wordpress.pdf"
 
 },{
   img:'/certifications/sence.png',
@@ -178,7 +178,7 @@ eng:[{
   },
   date:'February - 2024',
   color:'#e4e9f8',
-  url:"https://media.licdn.com/dms/image/v2/D4D2DAQFLemEu-ayqow/profile-treasury-document-images_800/B4DZaimavEGsAk-/1/1746484704901?e=1748476800&v=beta&t=kR7u7z3rlfC58skiMDT6e-MQnDF97m6AmmLFxGyNH-o"
+  url:"/files/ciberseguridad-1.pdf"
 
 },{
   img:'/certifications/IBM.png',
@@ -191,7 +191,7 @@ eng:[{
   },
   date:'January - 2025',
   color:'#dfdfdf',
-  url:"https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~QBHGPG24IVQB/CERTIFICATE_LANDING_PAGE~QBHGPG24IVQB.jpeg"
+  url:"/files/UX-UI.pdf"
 
 },{
   img:'/certifications/IBM.png',
@@ -204,7 +204,7 @@ eng:[{
   },
   date:'February - 2025',
   color:'#dfdfdf',
-  url:"https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~RPJQZRPNU4YW/CERTIFICATE_LANDING_PAGE~RPJQZRPNU4YW.jpeg"
+  url:"h/files/Bootstrap.pdf"
 
 },{
   img:'/certifications/Udemy.png',
@@ -216,7 +216,7 @@ eng:[{
   },
   date:'February - 2024',
   color:'#f0def8',
-  url:"https://media.licdn.com/dms/image/v2/D4D2DAQGL25op3SRMxQ/profile-treasury-document-images_800/B4DZXN7suTH4Ak-/1/1742916735343?e=1748476800&v=beta&t=UUf75TOjz5Xx9Mz21w-H-hyvJNMi4QeYno8PmmFqJ0E"
+  url:"/files/Certificado_Excel.pdf"
 },{
   img:'/certifications/ADL.png',
   type:0,
@@ -227,7 +227,7 @@ eng:[{
   },
   date:'November - 2024',
   color:'#e5f8cc',
-  url:"https://media.licdn.com/dms/image/v2/D4D2DAQHucHNa-H8O2w/profile-treasury-document-images_800/B4DZXN72nxHYAk-/1/1742916776796?e=1748476800&v=beta&t=JSCpnA99AEJQWowFdKvjXJ_8ttWm1v-81UgdmIOc6l4"
+  url:"/files/Figma.pdf"
 
 }, {
   img:'/certifications/UPLA.png',
@@ -240,7 +240,7 @@ eng:[{
   },
   date:'February - 2024',
   color:'#d7f8f4',
-  url:"https://media.licdn.com/dms/image/v2/D4D2DAQGK_9qbAjYmPA/profile-treasury-document-images_800/B4DZain.kbGsAk-/1/1746485113810?e=1748476800&v=beta&t=naudiot7OcnieSVxpnUQI-KYBuzGfBlLlXqeQq_bb0U"
+  url:"/files/Diplomado-UPLA.pdf"
 
 }, {
   img:'/certifications/AIEP.png',
@@ -253,7 +253,7 @@ eng:[{
   },
   date:'April - 2024',
   color:'#fae7e7',
-  url:"https://media.licdn.com/dms/image/v2/D4D2DAQHKha32mTdxQg/profile-treasury-document-images_800/B4DZainG7vGcAk-/1/1746484885385?e=1748476800&v=beta&t=yrNe_giJINM9XpEN5HCt5G7iWcVcptTJm5gQwF_NR-Y"
+  url:"/files/Diplomado-AIEP.pdf"
 
 }]
 }
--- src/assets/js/education.js
+++ src/assets/js/education.js
@@ -20,8 +20,8 @@ const education ={esp: [{
     active:true,
     title: 'Certificados',
     items:[
-      {name:'Backend Node y Express', url:'https://media.licdn.com/dms/image/v2/D4D2DAQH4cR92oS78hw/profile-treasury-image-shrink_800_800/B4DZWzt9ywG8AY-/0/1742476929388?e=1747940400&v=beta&t=fPMK7Ncz1-5N7hC5FmRVWM7QhpCAkvzbKvbB8-nkSQU'},
-      {name:'SQL', url:'https://media.licdn.com/dms/image/v2/D4D2DAQHUm1C7k8xtsA/profile-treasury-image-shrink_800_800/B4DZWzuDn2HYAc-/0/1742476953073?e=1747940400&v=beta&t=cgRKd3Oy9HNdlD2HkiHq1GvTIoUidunUmlZJZPNsraA'}
+      {name:'Backend Node y Express', url:'/files/Back-end.pdf'},
+      {name:'SQL', url:'/files/SQL.pdf'}
     ]}
 }, {
   img:'/education/ADL.png',
@@ -33,11 +33,11 @@ const education ={esp: [{
     active:true,
     title: 'Certificados',
     items:[
-      {name:'Frontend React', url:'https://media.licdn.com/dms/image/v2/D4D2DAQHwOa8O8ywWkA/profile-treasury-image-shrink_800_800/B4DZWzxPoUGcAk-/0/1742477788800?e=1747944000&v=beta&t=1Sd1J-mWTEoyAb4yHlxYfC5i7sD-vHJ7hMNqnNco1HU'},
-      {name:'React', url:'https://media.licdn.com/dms/image/v2/D4D2DAQH62RrbsGsaDw/profile-treasury-image-shrink_800_800/B4DZWzxFuwHkAY-/0/1742477748175?e=1747944000&v=beta&t=BXbbezoAyqISD4X-hNsJ264PIvWKQtOiLtk48FoJPo4'},
-      {name:'JavaScript para la Web', url:'https://media.licdn.com/dms/image/v2/D4D2DAQGlWAtWXAw5KA/profile-treasury-image-shrink_800_800/B4DZWzti1gHIAY-/0/1742476817311?e=1747944000&v=beta&t=Nl5wIWsp8AGWrH5NoqL8wmkHvj4Ll8a5r9RiHZEFFXw'},
-      {name:'CSS Avanzado', url:'https://media.licdn.com/dms/image/v2/D4D2DAQE5cS3BXPmF1Q/profile-treasury-image-shrink_800_800/B4DZWztd_0GkAY-/0/1742476799252?e=1747944000&v=beta&t=si-_-3uLTaS5ODmgQBeuaWsGGuoSHqbv3JRCzem0-v0'},
-      {name:'Introducción al desarrollo web', url:'https://media.licdn.com/dms/image/v2/D4D2DAQE_ec8UycjLhQ/profile-treasury-image-shrink_800_800/B4DZWzw7ZxHkAY-/0/1742477704521?e=1747944000&v=beta&t=g3iG9HlS_yn75j6wEWiK6NCoqzF5DmQhQGSTfmGBmyE'}
+      {name:'Frontend React', url:'/files/Front-end.pdf'},
+      {name:'React', url:'/files/React.pdf'},
+      {name:'JavaScript para la Web', url:'/files/JavaScript-Web.pdf'},
+      {name:'CSS Avanzado', url:'/files/CSS.pdf'},
+      {name:'Introducción al desarrollo web', url:'/files/Intro-Web.pdf'}
     ]}
 }, {
   img:'/education/Utokyo.png',
@@ -70,7 +70,7 @@ eng: [{
     active:true,
     title:'Certificates',
     items: [
-      {name:'FullStack Js', url:"https://media.licdn.com/dms/image/v2/D4D2DAQEV4aeC19oUzQ/profile-treasury-image-shrink_800_800/B4DZYlIb41HAAg-/0/1744379693880?e=1747944000&v=beta&t=ecD4UU8rGOuj00x3SY7lnKpE-2D_h9kbL6Hkj35P4Yg"}
+      {name:'FullStack Js', url:"/files/FullStack.pdf"}
     ]}
 }, {
   img:'/education/ADL.png',
@@ -82,8 +82,8 @@ eng: [{
     active:true,
     title: 'Certificates',
     items:[
-      {name:'Backend Node y Express', url:'https://media.licdn.com/dms/image/v2/D4D2DAQH4cR92oS78hw/profile-treasury-image-shrink_800_800/B4DZWzt9ywG8AY-/0/1742476929388?e=1747940400&v=beta&t=fPMK7Ncz1-5N7hC5FmRVWM7QhpCAkvzbKvbB8-nkSQU'},
-      {name:'SQL', url:'https://media.licdn.com/dms/image/v2/D4D2DAQHUm1C7k8xtsA/profile-treasury-image-shrink_800_800/B4DZWzuDn2HYAc-/0/1742476953073?e=1747940400&v=beta&t=cgRKd3Oy9HNdlD2HkiHq1GvTIoUidunUmlZJZPNsraA'}
+      {name:'Backend Node & Express', url:'/files/Back-end.pdf'},
+      {name:'SQL', url:'/files/SQL.pdf'}
     ]}
 }, {
   img:'/education/ADL.png',
@@ -95,11 +95,11 @@ eng: [{
     active:true,
     title: 'Certificates',
     items:[
-      {name:'Frontend React', url:'https://media.licdn.com/dms/image/v2/D4D2DAQHwOa8O8ywWkA/profile-treasury-image-shrink_800_800/B4DZWzxPoUGcAk-/0/1742477788800?e=1747944000&v=beta&t=1Sd1J-mWTEoyAb4yHlxYfC5i7sD-vHJ7hMNqnNco1HU'},
-      {name:'React', url:'https://media.licdn.com/dms/image/v2/D4D2DAQH62RrbsGsaDw/profile-treasury-image-shrink_800_800/B4DZWzxFuwHkAY-/0/1742477748175?e=1747944000&v=beta&t=BXbbezoAyqISD4X-hNsJ264PIvWKQtOiLtk48FoJPo4'},
-      {name:'JavaScript for the Web', url:'https://media.licdn.com/dms/image/v2/D4D2DAQGlWAtWXAw5KA/profile-treasury-image-shrink_800_800/B4DZWzti1gHIAY-/0/1742476817311?e=1747944000&v=beta&t=Nl5wIWsp8AGWrH5NoqL8wmkHvj4Ll8a5r9RiHZEFFXw'},
-      {name:'Advanced CSS', url:'https://media.licdn.com/dms/image/v2/D4D2DAQE5cS3BXPmF1Q/profile-treasury-image-shrink_800_800/B4DZWztd_0GkAY-/0/1742476799252?e=1747944000&v=beta&t=si-_-3uLTaS5ODmgQBeuaWsGGuoSHqbv3JRCzem0-v0'},
-      {name:'Introduction to web development', url:'https://media.licdn.com/dms/image/v2/D4D2DAQE_ec8UycjLhQ/profile-treasury-image-shrink_800_800/B4DZWzw7ZxHkAY-/0/1742477704521?e=1747944000&v=beta&t=g3iG9HlS_yn75j6wEWiK6NCoqzF5DmQhQGSTfmGBmyE'}
+      {name:'Frontend React', url:'/files/Front-end.pdf'},
+      {name:'React', url:'/files/React.pdf'},
+      {name:'JavaScript for the Web', url:'/files/JavaScript-Web.pdf'},
+      {name:'Advanced CSS', url:'/files/CSS.pdf'},
+      {name:'Introduction to web development', url:'/files/Intro-Web.pdf'}
     ]}
 }, {
   img:'/education/Utokyo.png',
